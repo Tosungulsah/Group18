f05a4a3bf6870ee448340c07d1cd4625bdd06246
Gulsah Tosun <gulsahustatosun@gmail.com>
2022-12-08 23:51:16 +0000

    changed in branch still

--- src/BranchPackge/NewBranchClass.java
+++ src/BranchPackge/NewBranchClass.java
@@ -4,5 +4,7 @@ public class NewBranchClass {
     public static void main(String[] args) {
         String word= "Wooden Spoon";
         System.out.println(word);
+        int cal= 123;
+        System.out.println(cal);
     }
 }
