44c045742a4a377ce93a4a26a18e554bcaafa2dd
Gulsah Tosun <gulsahustatosun@gmail.com>
2022-12-08 23:47:01 +0000

    Branch class

---
+++ src/BranchPackge/NewBranchClass.java
@@ -0,0 +1,8 @@
+package BranchPackge;
+
+public class NewBranchClass {
+    public static void main(String[] args) {
+        String word= "Wooden Spoon";
+        System.out.println(word);
+    }
+}
